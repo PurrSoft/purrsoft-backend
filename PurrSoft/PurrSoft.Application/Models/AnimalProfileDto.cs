387ba54ff67619fbff1b4e5dad440e7f03addff9
Dragota Robert <dragotarobert10@gmail.com>
2024-11-10 13:15:55 +0200

    PURRSOFT-54 Add AnimalProfileDto

---
+++ PurrSoft/PurrSoft.Application/Models/AnimalProfileDto.cs
@@ -0,0 +1,10 @@
+﻿namespace PurrSoft.Application.DTOs
+{
+    public class AnimalProfileDto
+    {
+        public Guid Id { get; set; }
+        public string? CurrentDisease { get; set; }
+        public string? CurrentMedication { get; set; }
+        public string? PastDisease { get; set; }
+    }
+}
