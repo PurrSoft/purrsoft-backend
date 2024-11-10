cf102da44202f9b2e44fc3341d233fa1fb38fa3e
Dragota Robert <dragotarobert10@gmail.com>
2024-11-10 13:16:36 +0200

    PURRSOFT-54 Add AnimalProfile queries

---
+++ PurrSoft/PurrSoft.Application/Queries/AnimalProfileQueries/AnimalProfileQueries.cs
@@ -0,0 +1,15 @@
+﻿using PurrSoft.Application.Common;
+using PurrSoft.Application.DTOs;
+using PurrSoft.Application.Models;
+
+namespace PurrSoft.Application.Queries.AnimalProfileQueries
+{
+    public class GetAnimalProfilesQuery : BaseRequest<CollectionResponse<AnimalProfileDto>>
+    {
+    }
+
+    public class GetAnimalProfileByIdQuery : BaseRequest<CommandResponse<AnimalProfileDto>>
+    {
+        public string Id { get; set; }
+    }
+}
---
+++ PurrSoft/PurrSoft.Application/Queries/AnimalProfileQueries/AnimalProfileQueryHandler.cs
@@ -0,0 +1,48 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using PurrSoft.Application.Common;
+using PurrSoft.Application.DTOs;
+using PurrSoft.Domain.Entities;
+using PurrSoft.Domain.Repositories;
+
+namespace PurrSoft.Application.Queries.AnimalProfileQueries
+{
+    public class AnimalProfileQueryHandler(IRepository<AnimalProfile> animalProfileRepository) :
+        IRequestHandler<GetAnimalProfilesQuery, CollectionResponse<AnimalProfileDto>>,
+        IRequestHandler<GetAnimalProfileByIdQuery, CommandResponse<AnimalProfileDto>>
+    {
+        public async Task<CollectionResponse<AnimalProfileDto>> Handle(GetAnimalProfilesQuery request, CancellationToken cancellationToken)
+        {
+            var result = await animalProfileRepository
+                .Query()
+                .Select(ap => new AnimalProfileDto
+                {
+                    Id = ap.Id,
+                    CurrentDisease = ap.CurrentDisease,
+                    CurrentMedication = ap.CurrentMedication,
+                    PastDisease = ap.PastDisease
+                })
+                .ToListAsync(cancellationToken);
+
+            return new CollectionResponse<AnimalProfileDto>(result, result.Count);
+        }
+
+        public async Task<CommandResponse<AnimalProfileDto>> Handle(GetAnimalProfileByIdQuery request, CancellationToken cancellationToken)
+        {
+            var profile = await animalProfileRepository
+                .Query(x => x.Id.ToString() == request.Id)
+                .Select(ap => new AnimalProfileDto
+                {
+                    Id = ap.Id,
+                    CurrentDisease = ap.CurrentDisease,
+                    CurrentMedication = ap.CurrentMedication,
+                    PastDisease = ap.PastDisease
+                })
+                .FirstOrDefaultAsync(cancellationToken);
+
+            return profile == null
+                ? CommandResponse.Failed<AnimalProfileDto>(new[] { "Animal profile not found." })
+                : CommandResponse.Ok(profile);
+        }
+    }
+}
