ca4e41d4297664528605c2fbfb642bb194b3e0f3
Domynis <54539177+Domynis@users.noreply.github.com>
2024-12-08 16:25:18 +0200

    PURRSOFT 59 - added a Guid validation method - enabled Guid validation check in Animal Validation for Update & Delete

--- PurrSoft/PurrSoft.Application/Helpers/DateTimeValidationHelper.cs
+++ PurrSoft/PurrSoft.Application/Helpers/DateTimeValidationHelper.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PurrSoft.Application.Helpers;
+﻿namespace PurrSoft.Application.Helpers;
 
 public static class DateTimeValidationHelper
 {
