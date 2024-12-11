2158db0c1088adef0223934ea653e092ee66854a
Domynis <54539177+Domynis@users.noreply.github.com>
2024-12-11 14:54:01 +0200

    PURRSOFT 77.1 - added Address string attribute to ApplicationUser and new migration for it (& snapshot) - added an Update Endpoint for AppUser - added an Update Roles Endpoint for AppUser

---
+++ PurrSoft/PurrSoft.Application/Commands/AccountCommands/AccountCommands.cs
@@ -0,0 +1,15 @@
+﻿using PurrSoft.Application.Common;
+using PurrSoft.Application.Models;
+
+namespace PurrSoft.Application.Commands.AccountCommands;
+
+public class UpdateAccountCommand : BaseRequest<CommandResponse>
+{
+	public ApplicationUserDto ApplicationUserDto { get; set; }
+}
+
+public class UpdateUserRolesCommand : BaseRequest<CommandResponse>
+{
+	public string? Id { get; set; }
+	public string[] Roles { get; set; }
+}
--- PurrSoft/PurrSoft.Application/Models/ApplicationUserDto.cs
+++ PurrSoft/PurrSoft.Application/Models/ApplicationUserDto.cs
@@ -3,10 +3,11 @@
 public class ApplicationUserDto
 {
     public string Id { get; set; }
-    public string DisplayName { get; set; }
+    public string? DisplayName { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public string Token { get; set; }
+    public string? Token { get; set; }
     public string? Email { get; set; }
-    public string[] Roles { get; set; }
+    public string? Address { get; set; }
+	public string[]? Roles { get; set; }
 }
