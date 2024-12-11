8d8071f1981837cbfa8ce9cbb690135d0053c512
Domynis <54539177+Domynis@users.noreply.github.com>
2024-12-12 01:24:34 +0200

    PURRSOFT 101 - added 2 new endpoints: Get Roles + Dates and Get Roles + Status for a UserId in AccountController

--- PurrSoft/PurrSoft.Api/Controllers/AccountController.cs
+++ PurrSoft/PurrSoft.Api/Controllers/AccountController.cs
@@ -12,27 +12,41 @@ namespace PurrSoft.Api.Controllers;
 [ApiController]
 public class AccountController : BaseController
 {
-    public AccountController()
-    {
+	public AccountController()
+	{
 
-    }
+	}
 
-    [HttpGet]
-    [Authorize(AuthenticationSchemes = "Bearer")]
-    [ProducesResponseType(typeof(ApplicationUserDto), (int)HttpStatusCode.OK)]
-    [ProducesResponseType(typeof(CommandResponse), (int)HttpStatusCode.BadRequest)]
-    public async Task<ApplicationUserDto> GetLoggedInUser()
-    {
-        ApplicationUserDto commandResponse =
-            await Mediator.Send(new GetLoggedInUserQuery());
+	[HttpGet]
+	[Authorize(AuthenticationSchemes = "Bearer")]
+	[ProducesResponseType(typeof(ApplicationUserDto), (int)HttpStatusCode.OK)]
+	[ProducesResponseType(typeof(CommandResponse), (int)HttpStatusCode.BadRequest)]
+	public async Task<ApplicationUserDto> GetLoggedInUser()
+	{
+		ApplicationUserDto commandResponse =
+			await Mediator.Send(new GetLoggedInUserQuery());
 
-        return commandResponse;
-    }
+		return commandResponse;
+	}
 
-    [HttpGet("GetUsersByRole/{role}")]
-    [Authorize(AuthenticationSchemes = "Bearer")]
-    [ProducesResponseType(typeof(ApplicationUserDto), (int)HttpStatusCode.OK)]
-    [ProducesResponseType(typeof(CommandResponse), (int)HttpStatusCode.BadRequest)]
-    public async Task<CollectionResponse<ApplicationUserDto>> GetLoggedInUserRole([FromRoute] string role)
-        => await Mediator.Send(new GetUsersByRoleQuery() { Role = role });
+	[HttpGet("{id}/GetRolesAndStatuses")]
+	[Authorize(AuthenticationSchemes = "Bearer")]
+	[ProducesResponseType(typeof(CollectionResponse<UserRoleStatusDto>), (int)HttpStatusCode.OK)]
+	[ProducesResponseType(typeof(CommandResponse), (int)HttpStatusCode.BadRequest)]
+	public async Task<CollectionResponse<UserRoleStatusDto>> GetRolesAndStatusesByUserId([FromRoute] string id)
+		=> await Mediator.Send(new GetRolesAndStatusesByUserIdQuery() { Id = id });
+
+	[HttpGet("{id}/GetRolesAndDates")]
+	[Authorize(AuthenticationSchemes = "Bearer")]
+	[ProducesResponseType(typeof(CollectionResponse<UserRoleDatesDto>), (int)HttpStatusCode.OK)]
+	[ProducesResponseType(typeof(CommandResponse), (int)HttpStatusCode.BadRequest)]
+	public async Task<CollectionResponse<UserRoleDatesDto>> GetRolesAndDatesByUserId([FromRoute] string id)
+		=> await Mediator.Send(new GetRolesAndDatesByUserIdQuery() { Id = id });
+
+	[HttpGet("GetUsersByRole/{role}")]
+	[Authorize(AuthenticationSchemes = "Bearer")]
+	[ProducesResponseType(typeof(ApplicationUserDto), (int)HttpStatusCode.OK)]
+	[ProducesResponseType(typeof(CommandResponse), (int)HttpStatusCode.BadRequest)]
+	public async Task<CollectionResponse<ApplicationUserDto>> GetLoggedInUserRole([FromRoute] string role)
+		=> await Mediator.Send(new GetUsersByRoleQuery() { Role = role });
 }
