5332e149539f5f9a8d755b3156c8a14b3c5eca01
Paula Haidamac <haidamacpaula@gmail.com>
2025-01-15 21:03:57 +0200

    PURRSOFT-145.1 Added Event & EventVolunteerMap entities

---
+++ PurrSoft/PurrSoft.Application/Commands/EventCommands/EventCommandValidator.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using PurrSoft.Application.Helpers;
+
+namespace PurrSoft.Application.Commands.EventCommands;
+
+public class EventCreateCommandValidator : AbstractValidator<CreateEventCommand>
+{
+    public EventCreateCommandValidator()
+    {
+        RuleFor(e => e.EventDto.Title).NotNull().NotEmpty();
+    }
+}
+
+public class EventUpdateCommandValidator : AbstractValidator<UpdateEventCommand>
+{
+    public EventUpdateCommandValidator()
+    {
+        RuleFor(e => e.EventDto.Id)
+            .NotNull().NotEmpty();
+        RuleFor(e => e.EventDto.Title)
+            .NotNull().NotEmpty();
+    }
+}
+
+public class EventDeleteCommandValidator : AbstractValidator<DeleteEventCommand>
+{
+    public EventDeleteCommandValidator()
+    {
+        RuleFor(e => e.Id)
+            .NotNull().NotEmpty()
+            .Must(GuidValidationHelper.BeAValidGuid)
+            .WithMessage("Id must be a valid GUID.");
+    }
+}
+
+
