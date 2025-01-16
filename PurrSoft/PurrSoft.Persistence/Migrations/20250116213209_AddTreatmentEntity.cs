dcd340885f79275d06c94535ef13f6ab6ce367a8
RobertDragota <dragotarobert10@gmail.com>
2025-01-16 23:37:19 +0200

    Update database snapshot

---
+++ PurrSoft/PurrSoft.Persistence/Migrations/20250116213209_AddTreatmentEntity.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PurrSoft.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTreatmentEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Treatments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    IdAnimal = table.Column<Guid>(type: "uuid", nullable: false),
+                    ImageUrl = table.Column<string>(type: "text", nullable: true),
+                    MedicationName = table.Column<string>(type: "text", nullable: false),
+                    MedicationTime = table.Column<string>(type: "text", nullable: false),
+                    ExtraInfo = table.Column<string>(type: "text", nullable: true),
+                    TreatmentStart = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    TreatmentEnd = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    TreatmentDays = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Treatments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Treatments_Animals_IdAnimal",
+                        column: x => x.IdAnimal,
+                        principalTable: "Animals",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Treatments_IdAnimal",
+                table: "Treatments",
+                column: "IdAnimal");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Treatments");
+        }
+    }
+}
--- PurrSoft/PurrSoft.Persistence/PurrSoftDbContextFactory.cs
+++ PurrSoft/PurrSoft.Persistence/PurrSoftDbContextFactory.cs
@@ -8,7 +8,7 @@ public class PurrSoftDbContextFactory : IDesignTimeDbContextFactory<PurrSoftDbCo
     public PurrSoftDbContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<PurrSoftDbContext>();
-        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=PurrSoft;Username=postgres;Password=postgres");
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=PurrSoft;Username=robert12;Password=Asmodeus011235");
         return new PurrSoftDbContext(optionsBuilder.Options);
     }
 }
